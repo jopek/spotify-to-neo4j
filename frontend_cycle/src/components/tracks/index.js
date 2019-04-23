b6d490de822cc410af16dfc4260b41237367567c
Alexander Bluem <abluem@testo.de>
2019-04-23 14:26:49 +0200

    add artist table css

--- frontend_cycle/src/components/tracks/index.js
+++ frontend_cycle/src/components/tracks/index.js
@@ -1,5 +1,5 @@
 import xs from 'xstream';
-import { table, tr, td, div } from '@cycle/dom';
+import { table, thead, tbody, tr, td, div } from '@cycle/dom';
 import { makeCollection } from '@cycle/state';
 import isolate from '@cycle/isolate';
 import sampleCombine from 'xstream/extra/sampleCombine';
@@ -19,57 +19,6 @@ const defaultState = {
             playlist: { name: 'Cosmic Chill', id: '3iRJ5ibkgFmKOE37EQOJMg' }
         },
         {
-            artist: { name: 'Pye Corner Audio', id: '3ib3ECT421EXd8CNLfNqAL' },
-            track: {
-                name: 'Mainframe',
-                track_number: 2,
-                id: '2uzrqj79NJ5QgSUzVo7ML6',
-                uri: 'spotify:track:2uzrqj79NJ5QgSUzVo7ML6',
-                preview_url:
-                    'https://p.scdn.co/mp3-preview/c6fc1e0828b808ec8f57f2f76999c538bb4fddb5?cid=774b29d4f13844c495f206cafdad9c86'
-            },
-            playlist: { name: 'Cosmic Chill', id: '3iRJ5ibkgFmKOE37EQOJMg' }
-        },
-        {
-            artist: { name: 'Suuns', id: '3UkN1XeK2D4wD4uhtJx4vb' },
-            track: {
-                name: 'Instrument',
-                track_number: 2,
-                id: '0Lv8ttBdPjhVTNytcrcumd',
-                uri: 'spotify:track:0Lv8ttBdPjhVTNytcrcumd',
-                preview_url:
-                    'https://p.scdn.co/mp3-preview/246c5534aec0014b774fa41c1b21abb6c819ea02?cid=774b29d4f13844c495f206cafdad9c86'
-            },
-            playlist: { name: 'Cosmic Chill', id: '3iRJ5ibkgFmKOE37EQOJMg' }
-        },
-        {
-            artist: { name: 'The Field', id: '23MIhFHpoOuhtEHZDrrnCS' },
-            track: {
-                name: "They Won't See Me",
-                track_number: 1,
-                id: '6g1B9a8BqGCJ7psysJX073',
-                uri: 'spotify:track:6g1B9a8BqGCJ7psysJX073',
-                preview_url:
-                    'https://p.scdn.co/mp3-preview/43ccc7ba5bbddad3b091b482d973ef068372452a?cid=774b29d4f13844c495f206cafdad9c86'
-            },
-            playlist: { name: 'Cosmic Chill', id: '3iRJ5ibkgFmKOE37EQOJMg' }
-        },
-        {
-            artist: {
-                name: 'Thievery Corporation',
-                id: '25KNo5GDS6ZpLkjasaecA3'
-            },
-            track: {
-                name: 'The Temple of I & I',
-                track_number: 6,
-                id: '113GeSf0XSbFZfyDRNrqWp',
-                uri: 'spotify:track:113GeSf0XSbFZfyDRNrqWp',
-                preview_url:
-                    'https://p.scdn.co/mp3-preview/0f650534eb99a8209809782111154c82ec146ff1?cid=774b29d4f13844c495f206cafdad9c86'
-            },
-            playlist: { name: 'Cosmic Chill', id: '3iRJ5ibkgFmKOE37EQOJMg' }
-        },
-        {
             artist: { name: 'Tosca', id: '0TYvluyvV1Es8lTHiBfnAn' },
             track: {
                 name: 'Supersunday - Megablast Remix',
@@ -85,9 +34,7 @@ const defaultState = {
 };
 
 const intent = ({ DOM }) => ({
-    clickArtist$: DOM.select('.artist')
-        .events('click')
-        .debug()
+    clickArtist$: DOM.select('.artist').events('click')
 });
 
 const model = intent => {
@@ -95,9 +42,7 @@ const model = intent => {
         prevState === undefined ? defaultState : prevState
     );
 
-    const c = intent.clickArtist$.map(e => state => state);
-
-    return xs.merge(initReducer$, c);
+    return xs.merge(initReducer$);
 };
 
 const Tracks = sources => {
@@ -112,14 +57,21 @@ const Tracks = sources => {
             return {
                 DOM: instances
                     .pickCombine('DOM')
-                    .map(itemVNodes => table(itemVNodes)),
+                    .map(itemVNodes =>
+                        table([
+                            thead(
+                                tr([td('artist'), td('title'), td('playlist')])
+                            ),
+                            tbody(itemVNodes)
+                        ])
+                    ),
                 state: instances.pickMerge('state'),
                 detailsRequest: instances.pickMerge('detailsRequest')
             };
         }
     });
 
-    const tracksTable = isolate(tracksCollection, { state: 'list' })(sources);
+    const tracksTable = isolate(tracksCollection, 'list')(sources);
 
     tracksTable.detailsRequest.debug();
 
@@ -146,9 +98,9 @@ const TrackItem = sources => {
         .map(([c, s]) => s)
         .map(s => s.artist);
 
-    xs.merge(state$, artistClick$, artistDetailsRequest$).subscribe({
-        next: x => x
-    });
+    // xs.merge(state$, artistClick$, artistDetailsRequest$).subscribe({
+    //     next: x => x
+    // });
 
     const vdom$ = state$.map(t =>
         tr([
