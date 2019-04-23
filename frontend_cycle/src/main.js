679c0ca8e69a21a63ce36ac2e4dbb66dd13a1447
Alexander Bluem <abluem@testo.de>
2019-04-23 14:25:19 +0200

    reproduce strange collection/lens behaviour

--- frontend_cycle/src/main.js
+++ frontend_cycle/src/main.js
@@ -6,9 +6,13 @@ import { withState } from '@cycle/state';
 import cli from './components/countlistitem';
 import cl from './components/countlist';
 import app from './components/app';
+import artist, { CollaboratorItem } from './components/artist';
+import wtf from './wtf_state';
 
+// const main = withState(wtf);
 const main = withState(app);
 // const main = withState(cl);
+// const main = withState(artist);
 
 // console.log(main)
 // main.detailsRequest.subscribe({next: x=>console.log(x)})
